61a3513aadc4ec2bb57467af9b704f673f4d69e2
AkbarHD <94240140+AkbarHD@users.noreply.github.com>
2022-02-20 00:37:56 -0800

    Add files via upload

---
+++ komparasi and logika.py
@@ -0,0 +1,61 @@
+print("============================kita akan mengkomparasi dan logika=========================    ")
+data1 = int(input("masukan angka\n kurang dari 0\n lebih dari 5\n kurang dari 8\n lebih dari 11 : "))
+kurangdari_0 = data1 < 0
+print("jawaban kurang dari 0 : ",kurangdari_0)
+lebihdari_5 = data1 > 5
+print("jawaban lebih dari 5 : ",lebihdari_5)
+kurangdari_8 = data1 < 8
+print("jawaban kurang dari 8 : ",kurangdari_8)
+lebihdari_11 = data1 > 11
+print("jawaban lebih dari 11 : ",lebihdari_11)
+print("\n",10*'=',"\n")
+data2 = int(input("masukan angka\nlebih dari 0\n kurang dari 5\n lebih dari 8\n kurang dari 11 : "))
+lebihdari_0 = data2 > 0
+print("jawaban lebih dari 0 : ",lebihdari_0)
+kurangdari_5 = data2 < 5
+print("jawaban kurang dari 5 : ",kurangdari_5)
+lebihdari_8 = data2 > 8
+print("maka jawaban lebih dari 8 : ",lebihdari_8)
+kurang_dari_11 = data2 < 11
+print("maka kurang dari 11 : ",kurang_dari_11)
+print("\n",10*"=","\n")
+data3 = int(input("masukan angka ya gaess yaa\n lebih dari 1000\n kurang dari 1000\n kurang dari 1500\n lebih dari 1500 : "))
+lebihDari_1000 = data3 > 1000
+print("jawaban lebih dari 1000 : ",lebihDari_1000)
+kurangDari_1000 = data3 < 1000
+print("jawaban kurang dari 1000 : ",kurangDari_1000)
+kurangDari_1500 = data3 < 1500
+print("jawaban kurang dari 1500 : ",kurangDari_1500)
+lebihDari_1500 = data3 > 15000
+print("jawaban lebih dari 1500 : ",lebihDari_1500)
+print("=======================Laporan Selesai================================================")
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
